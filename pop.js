6c558e7519bd84a8d4fc8363a2b458a1c6bbc3d4
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-08 19:40:42 +0530

    shift

---
+++ pop.js
@@ -0,0 +1,4 @@
+const colours = ["red", "green " ,"blue"];
+console.log(colours)
+colours.pop()// push method use to remove element from last
+console.log(colours)
---
+++ shift.js
@@ -0,0 +1,4 @@
+const colours = ["red", "green " ,"blue"];
+console.log(colours)
+colours.shift()// shift method use to remove element from first
+console.log(colours)
