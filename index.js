fcb9ee272300cf29bf15ebcb185bd6b640007174
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-08 19:34:06 +0530

    Length

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const Studnet = ["Rupesh",
+const Studnets = ["Rupesh",
     "nilesh",
     "Panakj",
     "Raj",
     "Harish"];
+console.log(Studnets)
+const val=Studnets.length
+console.log(`the length of array ${val}`)
