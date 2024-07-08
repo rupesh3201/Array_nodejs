9c6cdff1dcaa03898e976dae9981dc798e0cfa60
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-08 19:37:24 +0530

    Push

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const Studnets = ["Rupesh",
     "Raj",
     "Harish"];
 console.log(Studnets)
+//Length Method which counts the length of Array
 const val=Studnets.length
 console.log(`the length of array ${val}`)
---
+++ index1.js
@@ -0,0 +1,4 @@
+const colours = ["red", "green " ,"blue"];
+console.log(colours)
+colours.push("green")// push method use to add element in last
+console.log(colours)
