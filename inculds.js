d51890559bfdecbd645b6d388dafcbb626aba2ad
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-08 19:48:59 +0530

    includes

---
+++ inculds.js
@@ -0,0 +1,3 @@
+const colours = ["red", "green " ,"blue"];
+
+console.log(colours.includes("green"))// includes method use to check whether  element is present or not
--- slice.js
+++ slice.js
@@ -1,4 +1,4 @@
 const colours = ["red", "green " ,"blue"];
 console.log(colours)
-colours.slice(0,0,"PINK")// shift method use to remove element from first
+colours.slice(0,1,"PINK")// slice  method use to remove form (index ,del) element from whatever
 console.log(colours)
