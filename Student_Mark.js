2231fe81e512a615ca6bfa378f44972cb594e66a
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-09 19:34:39 +0530

    largest number

--- Student_Mark.js
+++ Student_Mark.js
@@ -1,2 +1,11 @@
 const Student_marks= [99,40,50,67,80,180]
 let temp_large = Student_marks[0];
+for(const num of Student_marks)
+    {
+        if(num>temp_large)
+        {
+            const largest_num = num;
+    
+        }
+    }
+    console.log(largest_num)
