820c52dc290bdb170cbd41c2d7bd747fcef345d8
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-14 19:44:06 +0530

    const largrest num

--- Largest_num_aaray.js
+++ Largest_num_aaray.js
@@ -3,5 +3,11 @@ let temp_large = Student_marks[0];
 for(const num of Student_marks) //for loop
 {
     if(num>temp_large)
+        {
+            const largest_num = num;
     
+        }
+}
+console.log(largest_num)
+
    
