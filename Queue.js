b0e831a843df47cb5ecb93afa5fb98a84ae9bd39
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-14 19:39:01 +0530

    Student Marks

---
+++ Queue.js
@@ -0,0 +1,10 @@
+const Queue = [];
+console.log( Queue )
+Queue.push("Rupesh");
+console.log( Queue );
+Queue.push("Harish");
+Queue.push("Nilesh");
+Queue.push("Raj");
+console.log( Queue );
+Queue.shift();
+console.log( Queue );
